cda6dc67d2a6d54e745ba36e6c92e78d2a907788
Albert <131771057+BertyAl@users.noreply.github.com>
2023-09-24 20:44:10 +0700

    Update main.dart
    tadi ada yang ke ganti,

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:project_uts/screens/homescreen.dart';
 import 'package:project_uts/utils/colors.dart';
 
 import 'package:project_uts/screens/log_in.dart';
 import 'package:project_uts/screens/Profile.dart';
 import 'package:project_uts/screens/sign_up.dart';
-=======
-import 'package:project_uts/screens/home_screen.dart';
-import 'package:project_uts/responsive/mobileLayout.dart';
-import 'package:project_uts/responsive/renponsiveLayout.dart';
-import 'package:project_uts/responsive/webLayout.dart';
-import 'package:project_uts/utils/colors.dart';
-// import 'package:project_uts/screens/log_in.dart';
-// import 'package:project_uts/screens/profile.dart';
-// import 'package:project_uts/screens/sign_up.dart';
-// import 'package:project_uts/screens/home_screen.dart';
->>>>>>> 76a13b61e2a749edf48dde56dddee74cfe7030ff
+
 
 void main() {
   runApp(const MyApp());
@@ -33,18 +22,11 @@ class MyApp extends StatelessWidget {
       title: 'HelloGram',
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-<<<<<<< HEAD
       // home: responsiveLayout(
       //   mobileScreenLayout: MobileLayout(),
       //   webScreenLayout: WebLayout(),
       // ),
       home: HomeScreen(),
-=======
-      home: ResponsiveLayout(
-        mobileScreenLayout: MobileLayout(),
-        webScreenLayout: WebLayout(),
-      ),
->>>>>>> 76a13b61e2a749edf48dde56dddee74cfe7030ff
     );
   }
 }
